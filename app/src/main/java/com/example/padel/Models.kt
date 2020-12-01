9d888722b3fc775ca6476e3da1f19960f7b5b7be
Francisco Revilla <paconte@gmail.com>
2020-12-01 20:37:03 +0100

    Add app folder with sources.

---
+++ app/src/main/java/com/example/padel/Models.kt
@@ -0,0 +1,39 @@
+package com.example.padel
+
+
+class RankingList (
+        val title: String,
+        val series: String,
+        val federation: String,
+        val division: String,
+        val date: String) {
+
+    fun getFlagImage() : Int {
+        var result : Int = -1
+
+        when(this.title.toLowerCase()) {
+            "germany" -> result = R.drawable.germany
+            "netherlands" -> result = R.drawable.netherlands
+            "switzerland" -> result = R.drawable.switzerland
+            "thailand" -> result = R.drawable.thailand
+            "world padel tour" -> result = R.drawable.international_fed
+        }
+
+        return result
+    }
+
+    fun hrDivision() : String {
+        var result = ""
+
+        when(this.division) {
+            "MO" -> result = "Men Open"
+            "WO" -> result = "Women Open"
+            "O" -> result = "Men and Women Open"
+        }
+        return result
+    }
+}
+
+class RankingRow(val position: Int, val person: RankingPerson, val points: Int)
+
+class RankingPerson(val first_name: String, val last_name: String)
---
+++ app/src/main/java/com/example/padel/RankingActivity.kt
@@ -0,0 +1,52 @@
+package com.example.padel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
+class RankingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+        val title = intent.getStringExtra(RankingListViewHolder.RANKING_TITLE)
+        val federation = intent.getStringExtra(RankingListViewHolder.RANKING_FEDERATION)
+        val division = intent.getStringExtra(RankingListViewHolder.RANKING_DIVISION)
+        val date = intent.getStringExtra(RankingListViewHolder.RANKING_DATE)
+        supportActionBar?.title = title
+        println("${title}, ${federation}, ${division}, ${date}")
+        fetchJson(federation, division, date)
+    }
+
+    private fun fetchJson(federation : String?, division : String?, date : String?) {
+        val url = "https://www.padelanalytics.com/api/ranking?federation=${federation}&division=${division}&date=${date}"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+                val gson = GsonBuilder().create()
+                val rankingFeed : Array<RankingRow> =
+                        gson.fromJson(body, Array<RankingRow>::class.java)
+                //recyclerViewRanking.adapter = RankingAdapter(rankingFeed)
+                runOnUiThread {
+                    recyclerViewRanking.adapter = RankingAdapter(rankingFeed)
+                }
+
+            }
+        })
+    }
+
+}
